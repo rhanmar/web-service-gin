d7b277a03f9cd2c7d913756f01473ec7ff91b4df
dmitriy <ovchinnikovdk@yandex.ru>
2023-11-04 12:21:23 +0700

    feat: добавил подключение к postgres

--- pkg/config/database.go
+++ pkg/config/database.go
@@ -2,17 +2,33 @@ package config
 
 import (
 	"example/web-service-gin/pkg/models"
+	"gorm.io/driver/postgres"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
-func ConnectDatabase() {
+func connectSqlite() (db *gorm.DB, err error) {
 	database, err := gorm.Open(sqlite.Open("my_db.db"), &gorm.Config{})
 	if err != nil {
 		panic("Failed to connect to database!")
 	}
+	return database, err
+}
+
+func connectPostgres() (db *gorm.DB, err error) {
+	dsn := "postgres://postgres:postgres@localhost:15222"
+	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("Failed to connect to database!")
+	}
+	return database, err
+}
+
+func ConnectDatabase() {
+	//database, _ := connectSqlite()
+	database, _ := connectPostgres()
 	database.AutoMigrate(&models.Album{})
 	DB = database
 }
